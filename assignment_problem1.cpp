e29727353d0a8899c700c243d2544b83659e8cbc
Shiwani Dhangar <115006724+shivu2k22@users.noreply.github.com>
2022-10-15 12:37:34 +0530

    Finding Greater number

---
+++ assignment_problem1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<<"Enter two numbers:";
+    cin>>n1>>n2;
+    if(n1>n2)
+    cout<<"Greatre number is: "<<n1;
+    if(n2>n1)
+    cout<<"Greatre number is: "<<n2;
+    return 0;
+
+}
